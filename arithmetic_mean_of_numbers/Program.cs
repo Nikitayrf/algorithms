acfcad9b5cf35296f6ffe0ca917b73ede4cc8356
Nikitayrf <samaraphone@yandex.ru>
2022-11-12 17:52:42 +0100

    add arithmetic mean of numbers

---
+++ arithmetic_mean_of_numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти среднее арифметическое введенных чисел
+
+Console.Write("Введите сколько будет чисел : ");
+
+int elementsCounts = Convert.ToInt32(Console.ReadLine());
+
+double[] numbers = new double[elementsCounts];
+double result = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+   Console.Write($"Введите {i + 1}-е число: ");
+   numbers[i] = Convert.ToInt32(Console.ReadLine());
+   result = numbers[i] + result; 
+}
+double arithmeticMean = result / numbers.Length;
+
+Console.WriteLine($"Среднеарифметическое равно: {arithmeticMean}");
