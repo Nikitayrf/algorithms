26b30ab3e3cb1637942a00e377306f7c32decd24
Nikitayrf <samaraphone@yandex.ru>
2022-11-12 21:21:09 +0100

    add info about output array to console

---
+++ output_array_to_console/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вывести массив в консоль
+
+Console.Write("Введите сколько чисел будет в массиве : ");
+
+int elementsCounts = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[elementsCounts];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"Введите {i + 1}-е число массива : ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Вывод массива: \t");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{numbers[i]} ");
+}
