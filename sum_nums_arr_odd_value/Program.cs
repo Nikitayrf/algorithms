8069b456009590d4ec1eceaa81a3bc8d0a2d1e16
Nikitayrf <samaraphone@yandex.ru>
2022-11-15 16:09:16 +0100

    add info about finding the sum of an array with an odd value

---
+++ sum_nums_arr_odd_value/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму элементов массива с нечетным значением
+
+int[] numbers = { 1, 12, 3, 4, 5, 16, 7, 8, 9 };
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++){
+    if (numbers[i] % 2 == 1){
+        sum += numbers[i];
+    }
+}
+
+Console.WriteLine($"Сумма элементов с нечетным значением равна : {sum}");
