a87e22f406c41780b088f33c2f4b13c9ae7b5057
Nikitayrf <samaraphone@yandex.ru>
2022-11-15 11:57:30 +0100

    add about finding scalar product

---
+++ scalar_product/Program.cs
@@ -0,0 +1,17 @@
+﻿// Записать алгоритм нахождения скалярного произведения двух векторов.
+// Формула: (a, b) × (c, d) = a × c + b × d
+
+int[] firstVector = { 1, 8 };
+int[] secondVector = { 6, 3 };
+
+int size = firstVector.Length;
+int scalarProduct = 0;
+int index = 0;
+
+while (index < size)
+{
+    scalarProduct = scalarProduct + firstVector[index] * secondVector[index];
+    index++;
+}
+
+Console.WriteLine(scalarProduct);
