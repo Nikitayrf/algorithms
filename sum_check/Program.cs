576743dd119be8363dc6c262af7920c861897f4a
Nikitayrf <samaraphone@yandex.ru>
2022-11-15 12:59:08 +0100

    add info about finding sum check

---
+++ sum_check/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вычисление суммы чека
+
+int[] price = {99, 100, 60}; // массив цен на продукты
+int[] numberProduct = {2, 3, 1}; // массив колличества того или иного продукта
+int numberItems = price.Length; // сколько позиций в чеке, длинна массива
+int index = 0;
+int sum = 0; // сумма чека
+
+while (index < numberItems)
+{
+    sum = sum + price[index] * numberProduct[index];
+    index++;
+}
+
+Console.WriteLine(sum);
