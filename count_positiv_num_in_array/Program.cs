7c7dbab2ab9592ebdd743019d07862a4d59bcd83
Nikitayrf <samaraphone@yandex.ru>
2022-11-22 11:52:54 +0100

    add info about finding the number of positive numbers in random array

---
+++ count_positiv_num_in_array/Program.cs
@@ -0,0 +1,39 @@
+﻿// Посчитать количество положительных чисел в произвольно заданном массиве.
+
+void someArray(int[] argument) // метод генерирует массив случайных чисел (-10, 10)
+
+{
+    for (int i = 0; i < argument.Length; i++)
+    {
+        argument[i] = new Random().Next(-10, 10);
+    }
+}
+
+void printArray(int[] col) // метод вывод к консоль массив
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write($"{col[i]} ");
+    }
+}
+
+void positivNum(int[] array) // метод считает кол-во положительных чисел в массиве
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        } 
+    }
+    Console.WriteLine($"\nКолличество положительных символов равно : {count}");
+}
+
+int[] array = new int[10];
+
+someArray(array);
+printArray(array);
+positivNum(array);
+
+
