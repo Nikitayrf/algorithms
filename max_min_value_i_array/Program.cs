e919d662ef64dd653d5eb286d37b0850eccd5e77
Nikitayrf <samaraphone@yandex.ru>
2022-11-22 12:01:04 +0100

    add info about finding max min value and index in array

---
+++ max_min_value_i_array/Program.cs
@@ -0,0 +1,41 @@
+﻿// Найти максимальный и минимальный элемент массива и их индексы.
+
+void someArray(int[] argument) // метод генерирует массив случайных чисел (-10, 10)
+
+{
+    for (int i = 0; i < argument.Length; i++) 
+    {
+        argument[i] = new Random().Next(-10, 10);
+    }
+}
+
+int[] numbers = new int[10];
+
+someArray(numbers);
+
+for (int i = 0; i < numbers.Length; i++) // вывести все значения массива в консоль
+{
+    Console.Write($"{numbers[i]} ");
+}
+
+int max = numbers[0];
+int min = numbers[0];
+int imax = 0;
+int imin = 0;
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (numbers[i] >  max){
+        max = numbers[i];
+        imax = i;
+    }
+    else
+    {
+        if (numbers[i] < min) {
+            min = numbers[i];
+            imin = i;
+        }
+    }
+}
+
+Console.WriteLine($"\nimax = {imax}, imin = {imin}, max = {max}, min = {min}");
