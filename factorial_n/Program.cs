012d318c39632947d9e1335a59090403183bdbae
Nikitayrf <samaraphone@yandex.ru>
2022-11-22 11:54:24 +0100

    add info about finding factorial number n

---
+++ factorial_n/Program.cs
@@ -0,0 +1,20 @@
+﻿// Метод вычисления факториала n!.
+
+Console.Write("Введите целое положительное число факториал которого вы хотите найти : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorialN(int num)
+{
+    int factorial = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        factorial = factorial * i;
+    }
+    Console.WriteLine($"Факториал равен : {factorial}");
+    return factorial;
+    
+}
+
+factorialN(number);
+
+
