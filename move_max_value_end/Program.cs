0cbc1948b9dc0f60003e9bf5c6be1a0f4b547542
Nikitayrf <samaraphone@yandex.ru>
2022-11-22 12:03:07 +0100

    add info about move the maximum element to the end of the array

---
+++ move_max_value_end/Program.cs
@@ -0,0 +1,21 @@
+﻿// "Перенести" максимальное значение в массиве в конец. (Пузырьковая сортировка)
+
+int[] numbers = { 1, 3, -3, -4, 5, 9, 6, -3, 8, -1 };
+
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int temp;
+    for (int j = 0; j < numbers.Length - 1 - i; j++)
+        if (numbers[j] > numbers[j + 1])
+        {
+            temp = numbers[j];
+            numbers[j] = numbers[j + 1];
+            numbers[j + 1] = temp;
+        }
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{numbers[i]} ");
+}
